bdc234f236aec040d094ab5490d33a1ef0084afb
Chanan Braunstein <chanan@bkbunch.com>
2020-04-02 17:51:51 -0600

    League font

---
+++ src/BlazorSlides/Internal/Components/Themes/League/Headers.razor.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Components;
+using System.Threading.Tasks;
+
+namespace BlazorSlides.Internal.Components.Themes.League
+{
+    public partial class Headers : ComponentBase
+    {
+        [Parameter] public string RevealClassname { get; set; }
+    }
+}
--- src/BlazorSlides/Theme.cs
+++ src/BlazorSlides/Theme.cs
@@ -3,6 +3,7 @@
     public enum Theme
     {
         Black,
-        White
+        White,
+        League
     }
 }
