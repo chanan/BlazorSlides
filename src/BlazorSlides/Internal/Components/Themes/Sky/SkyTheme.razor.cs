02f6a4061a29cdae99c4aa8413ce5ebf2953a8b0
Chanan Braunstein <chanan@bkbunch.com>
2020-04-02 18:19:47 -0600

    Sky theme

---
+++ src/BlazorSlides/Internal/Components/Themes/Sky/SkyTheme.razor.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Components;
+using System.Threading.Tasks;
+
+namespace BlazorSlides.Internal.Components.Themes.Sky
+{
+    public partial class SkyTheme : ComponentBase
+    {
+        private string _classname;
+        private bool _rendered = false;
+        [Parameter] public Theme Theme { get; set; }
+        [Parameter] public Region Region { get; set; }
+        [Parameter] public string Classname { get; set; }
+        [Parameter] public EventCallback<string> ClassnameChanged { get; set; }
+        [Parameter] public string RevealClassname { get; set; }
+        [Parameter] public string ProgressClassname { get; set; }
+        [Parameter] public string ControlsClassname { get; set; }
+
+        protected override async Task OnAfterRenderAsync(bool firstRender)
+        {
+            if (_classname != null && !_rendered)
+            {
+                _rendered = true;
+                Classname = _classname;
+                await ClassnameChanged.InvokeAsync(Classname);
+            }
+        }
+    }
+}
--- src/BlazorSlides/Theme.cs
+++ src/BlazorSlides/Theme.cs
@@ -3,7 +3,8 @@
     public enum Theme
     {
         Black,
-        White,
-        League
+        League,
+        Sky,
+        White
     }
 }
